d123401c3f0757c3b9813a6753553552764e447f
Tim Woo <Tim Woo>
2017-06-08 00:43:20 -0400

    this is a new commit

---
+++ database/home.php
@@ -0,0 +1,11 @@
+<?php
+	echo '<h1> Assignment 4 </h1> -
+		<ol> - 
+		<li><a href = "Clients.php">Clients</a><br></li> - 
+		<li><a href = "Branch.php">Branch</a><br></li> - 
+		<li><a href = "Insurance.php">Insurance</a><br></li> - 
+		<li><a href = "Mileage.php">Mileage</a><br></li> - 
+		<li><a href = "Reservations.php">Reservations</a><br></li> - 
+		</ol>';
+?>
+	
