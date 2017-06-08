566b639b99657db2d93cea873b819bd5fd6ba722
Tim Woo <Tim Woo>
2017-06-08 02:31:02 -0400

    fix10 clients.php

--- database/Clients.php
+++ database/Clients.php
@@ -1,11 +1,5 @@
-<ol>
-	<li><a href = "http://webtech.kettering.edu/~woo2733/Assignment4/database/database/home.php">Home</a><br></li>
-	<li><a href = "http://webtech.kettering.edu/~woo2733/Assignment4/database/database/Clients.php">Clients</a><br></li>
-	<li><a href = "http://webtech.kettering.edu/~woo2733/Assignment4/database/database/Branch.php">Branch</a><br></li>
-	<li><a href = "http://webtech.kettering.edu/~woo2733/Assignment4/database/database/Insurance.php">Insurance</a><br></li>
-	<li><a href = "http://webtech.kettering.edu/~woo2733/Assignment4/database/database/Mileage.php">Mileage</a><br></li>
-	<li><a href = "http://webtech.kettering.edu/~woo2733/Assignment4/database/database/Reservations.php">Reservations</a><br></li>
-</ol>
+<h1> Clients </h1>
+<li><a href = "http://webtech.kettering.edu/~woo2733/Assignment4/database/database/home.php">Home</a><br></li>
 
 <?php
 	include_once 'dbconfig.php';
@@ -14,31 +8,30 @@
 
 <html>
 <head>
-	<title>Clients</title>
 </head>
 <body>
+	<table width='50%' border=1>
+		<tr bg color='#CCCCCC'>
+			<td>ID</td>
+			<td>Name</td>
+			<td>Address</td>
+			<td>Phone</td>
+			<td>License</td>
+			<td>From Corporation</td>
+		</tr>
+		<?php
+			while($res = mysqli_fetch_array($result)){
+				echo "<tr>
+					<td>".$res['cid']."</td>
+					<td>".$res['cname']."</td>
+					<td>".$res['cadd']."</td>
+					<td>".$res['cphone']."</td>
+					<td>".$res['license']."</td>
+					<td>".$res['iscorp']."</td>
+					</tr>";
+			}
+		?>
+	</table><br>
 	<a href="Clients_add.html">Add New Clients</a><br/><br/>
-		<table width='50%' border=1>
-			<tr bg color='#CCCCCC'>
-				<td>ID</td>
-				<td>Name</td>
-				<td>Address</td>
-				<td>Phone</td>
-				<td>License</td>
-				<td>From Corporation</td>
-			</tr>
-			<?php
-				while($res = mysqli_fetch_array($result)){
-					echo "<tr>
-						<td>".$res['cid']."</td>
-						<td>".$res['cname']."</td>
-						<td>".$res['cadd']."</td>
-						<td>".$res['cphone']."</td>
-						<td>".$res['license']."</td>
-						<td>".$res['iscorp']."</td>
-						</tr>";
-				}
-			?>
-		</table>
 </body>
 </html>
---
+++ database/client_add.php
@@ -0,0 +1,44 @@
+<h1>New Client</h1>
+<?php
+
+	include_once("dbconfig.php");
+	
+	if(isset($_POST['Submit'])){
+		$cname = mysqli_real_escape_string($mysqli, $_POST['cname']);
+		$cadd = mysqli_real_escape_string($mysqli, $_POST['cadd']);
+		$cphone = mysqli_real_escape_string($mysqli, $_POST['cphone']);
+		$license = mysqli_real_escape_string($mysqli, $_POST['license']);
+
+		// checking empty fields
+		if(empty($cname) || empty($cadd) || empty($cphone) || empty($license)) {
+
+			if(empty($cname)) {
+				echo "<font color='red'>Name field is empty.</font><br/>";
+			}
+
+			if(empty($cadd)) {
+				echo "<font color='red'>Address field is empty.</font><br/>";
+			}
+
+			if(empty($cphone)) {
+				echo "<font color='red'>Phone field is empty.</font><br/>";
+			}
+
+			if(empty($license)) {
+				echo "<font color='red'>License Number field is empty.</font><br/>";
+			}
+
+			//link to the previous page
+			echo "<br/><a href='javascript:self.history.back();'>Go Back</a>";
+		} else {
+			// if all the fields are filled (not empty)
+
+			//insert data to database
+			$result = mysqli_query($mysqli, "INSERT INTO Clients (cname, cadd, cphone, license) VALUES ('$cname', '$cadd', '$cphone', '$license')");
+			
+			//display success message
+			echo "<font color='green'>Data added successfully.";
+			echo "<br/><a href='Clients.php'>View Result</a>";
+		}
+	}
+?>
